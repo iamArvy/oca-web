544a06027e2ccc3dc357ffadc3cdd098320c117c
Oluwaseyi Oke <125017904+iamArvy@users.noreply.github.com>
2026-03-30 14:34:06 +0100

    fix(post): fix image styling in article to adjust rss images

--- app/components/post/content/article.vue
+++ app/components/post/content/article.vue
@@ -5,8 +5,9 @@ defineProps<{
 </script>
 
 <template>
-  <article
-    class="prose prose-lg max-w-3xl text-foreground prose-a:text-foreground prose-strong:text-foreground prose-img:max-w-full prose-img:h-auto prose-img:rounded-lg prose-img:mx-auto">
+  <article class="prose prose-lg max-w-3xl text-foreground prose-a:text-foreground prose-strong:text-foreground prose-img:max-w-full prose-img:h-auto prose-img:rounded-lg prose-img:mx-auto  [&_img]:max-w-full
+         [&_img]:h-auto
+         [&_img]:object-contain">
     <div v-html="htmlContent" />
   </article>
 </template>
