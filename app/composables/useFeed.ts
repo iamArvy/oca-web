5f199bc7ad6be11e71af1e682c30a6fca54ac1a4
Oluwaseyi Oke <125017904+iamArvy@users.noreply.github.com>
2026-03-30 13:13:52 +0100

    fix(feed): issue with autoscroll in production

--- app/composables/useFeed.ts
+++ app/composables/useFeed.ts
@@ -1,67 +1,3 @@
-// import { ref, unref, watch } from "vue";
-// import type { ApiListResponse, Posts } from "~/interfaces";
-
-// export function useFeed(route: string, query?: MaybeRef<Record<string, any>>) {
-//   const currentPage = ref(1);
-//   const loading = ref(false);
-//   const hasNextPage = ref(false);
-//   const posts = ref<Posts>([]);
-//   const count = ref(0);
-
-//   const fetch = async (page = currentPage.value, reset = false) => {
-//     if (loading.value) return;
-//     loading.value = true;
-
-//     try {
-//       const { data } = await useAPI<ApiListResponse<Posts>>(route, {
-//         query: {
-//           ...unref(query),
-//           page,
-//         },
-//       });
-
-//       if (reset) {
-//         posts.value = data.value?.data ?? [];
-//       } else {
-//         posts.value.push(...(data.value?.data ?? []));
-//       }
-
-//       hasNextPage.value = data.value?.meta?.has_next ?? false;
-//       count.value = data.value?.meta?.total ?? 0;
-//     } finally {
-//       loading.value = false;
-//     }
-//   };
-
-//   const loadMore = async () => {
-//     if (!hasNextPage.value) return;
-//     currentPage.value++;
-//     await fetch(currentPage.value);
-//   };
-
-//   // Initial fetch
-//   fetch(1, true);
-
-//   // Reactive query watcher
-//   watch(
-//     () => unref(query),
-//     () => {
-//       currentPage.value = 1;
-//       fetch(1, true);
-//     },
-//     { deep: true }
-//   );
-
-//   return {
-//     loading,
-//     posts,
-//     count,
-//     hasNextPage,
-//     currentPage,
-//     loadMore,
-//   };
-// }
-
 import type { ApiListResponse, Posts } from "~/interfaces";
 import { API_ROUTES } from "~/constants";
 export function useFeed(query?: MaybeRef<Record<string, any>>) {
@@ -109,10 +45,10 @@ export function useFeed(query?: MaybeRef<Record<string, any>>) {
 
   onMounted(fetch);
 
-  const loadMore = async () => {
+  const loadMore = () => {
     if (!hasNextPage.value || loading.value) return;
     currentPage.value++;
-    await fetch();
+    fetch();
   };
 
   return {
