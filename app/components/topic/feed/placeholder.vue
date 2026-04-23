4a21c74cd888453542119ceb762ee623857cb4fd
Oluwaseyi Oke <125017904+iamArvy@users.noreply.github.com>
2026-04-23 11:02:04 +0100

    feat: adjust Skeleton component height in topic feed placeholder for improved layout

--- app/components/topic/feed/placeholder.vue
+++ app/components/topic/feed/placeholder.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="py-8">
-    <Skeleton class="w-30 h-20" />
+    <Skeleton class="w-30 h-5" />
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 stagger-children">
       <PostCardPlaceholder v-for="n in 3" :key="n" />
     </div>
