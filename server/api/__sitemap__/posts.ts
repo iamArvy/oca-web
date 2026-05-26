74f802d7b75877e7a35b5f4964b1ed18558ff066
Oluwaseyi Oke <125017904+iamArvy@users.noreply.github.com>
2026-05-26 14:38:54 +0100

    fix: comment out unused imports and API call in sitemap posts handler

--- server/api/__sitemap__/posts.ts
+++ server/api/__sitemap__/posts.ts
@@ -1,16 +1,21 @@
-import { ApiListResponse, Post } from '~/interfaces'
-import { API_ROUTES } from '~/constants'
-import { createApi } from '~~/server/utils/api'
+// import { ApiListResponse, Post } from '~/interfaces'
+// import { API_ROUTES } from '~/constants'
 
 export default defineSitemapEventHandler(async (event) => {
-  const api = createApi(event)
+  // const api = createApi(event)
 
-  const posts = await api<ApiListResponse<Post>>(API_ROUTES.posts.path, {
-    query: {
-      limit: 5000,
-      days: 7,
-    }
-  })
+  // const posts = await api<ApiListResponse<Post>>(API_ROUTES.posts.path, {
+  //   query: {
+  //     limit: 100,
+  //     days: 7,
+  //   }
+  // })
+
+  const posts = {
+  data: [
+    { slug: 'test', updatedAt: new Date() }
+  ]
+}
 
   return posts.data.map((post) => ({
     loc: `/posts/${post.slug}`,
